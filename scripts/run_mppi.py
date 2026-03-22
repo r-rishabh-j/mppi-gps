9893db115a8546b74bd3e4ce348a18cc877613b1
Jaisel Singh <singhjaisel01@gmail.com>
2026-03-21 23:17:34 -0400

    finished mppi

---
+++ scripts/run_mppi.py
@@ -0,0 +1,5 @@
+import numpy as np 
+from src.envs.half_cheetah import HalfCheetah
+from src.mppi.mppi import MPPI
+from src.utils.config import MPPIConfig
+
--- src/mppi/mppi.py
+++ src/mppi/mppi.py
@@ -1,2 +1,122 @@
 """information theoretic mppi (2018 Williams et al.)"""
+import numpy as np 
+from src.envs.base import BaseEnv 
+from src.utils.config import MPPIConfig
+from src.utils.math import (
+    compute_weights, effective_sample_size, gaussian_log_prob
+)
+
+class MPPI:
+    
+    def __init__(self, env: BaseEnv, cfg: MPPIConfig):
+        self.env = env 
+        self.cfg = cfg
+        self.K = cfg.K 
+        self.H = cfg.H
+        self.lam = cfg.lam
+        self.sigma = cfg.noise_sigma
+
+        self.nu = env.action_dim
+        self.act_low, self.act_high = env.action_bounds
+
+        self.U = np.zeros((self.H, self.nu))
+
+        self._last_states = None
+        self._last_actions = None
+        self._last_weights = None
+        self._last_costs = None
+
+    def reset(self):
+        self.U = np.zeros((self.H, self.nu))
+
+    def plan_step(
+            self, 
+            state: np.ndarray, 
+            prior = None,
+    ) -> tuple[np.ndarray, dict]:
+        """running one MPPI iteration
+        state: current environment state 
+        prior: this is an optional callable (states, actions) -> log_prob (K, )
+        """
+        # sample from q = N(U_nominal, sigma^2 I)
+        # noise is eps 
+        eps = np.random.randn(self.K, self.H, self.nu) * self.sigma 
+        U_perturbed = self.U[None, :, :] + eps 
+
+        # clamp before rollout 
+        np.clip(U_perturbed, self.act_low, self.act_high, out = U_perturbed)
+        
+        # rollout 
+        states, costs = self.env.batch_rollout(state, U_perturbed)
+
+        # compute the prior/ proposal 
+        log_prior = None 
+        log_proposal = None 
+
+        if prior is not None:
+            log_prior = prior(states, U_perturbed)
+            log_proposal = gaussian_log_prob(U_perturbed, self.U, self.sigma)
+
+        # compute weights 
+        lam = self.lam 
+        weights = compute_weights(costs, lam, log_prior, log_proposal)
+        n_eff = effective_sample_size(weights)
+        
+        # you want to make sure that the weights don't collapse aka lambda is not too small 
+        # if lambda is small then the policy isn't exploring
+        if self.cfg.adaptive_lam:
+            for _ in range(5):
+                if n_eff < self.cfg.n_eff_threshold:
+                    lam *= 2.0
+                elif n_eff > 0.75 * self.K:
+                    lam *= 0.5 
+                else:
+                    break 
+                lam = np.clip(lam, 0.01, 100.0)
+                weights = compute_weights(costs, lam, log_prior, log_proposal)
+                n_eff = effective_sample_size(weights)
+            self.lam = lam 
+
+        # compute the weighted mean 
+        self.U = np.einsum('k, kha -> ha', weights, U_perturbed)
+        np.clip(self.U, self.act_low, self.act_high, out = self.U)
+
+        # extract action 
+        action = self.U[0].copy()
+
+        # shift horizon 
+        self.U[:-1] = self.U[1:]
+        self.U[-1] = 0.0 
+
+        # store for GPS 
+        self._last_states = states
+        self._last_actions = U_perturbed
+        self._last_weights = weights
+        self._last_costs = costs
+
+        info = {
+            'cost_mean': np.mean(costs),
+            'cost_min': np.min(costs),
+            'n_eff': n_eff,
+            'lam': lam,
+        }
+        return action, info
+    
+    def get_rollout_data(self) -> dict:
+        return {
+            'states': self._last_states,
+            'actions': self._last_actions,
+            'weights': self._last_weights,
+            'costs': self._last_costs,
+            }
+    
+
+
+
+                
+                
+
+
+
+
 
